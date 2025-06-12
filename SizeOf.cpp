124be16045dbc70408dc7526b540ecaff6ea3763
balwantsinghmnit <balwantsinghmnit@gmail.com>
2025-06-12 16:28:06 +0530

    sizeof operator

---
+++ SizeOf.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Printing the size of each data type
+    cout << "Size of bool: " << sizeof(bool) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of short: " << sizeof(short) << " bytes" << endl;
+    cout << "Size of long: " << sizeof(long) << " bytes" << endl;
+    cout << "Size of long long: " << sizeof(long long) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes";
+
+    return 0;
+}
